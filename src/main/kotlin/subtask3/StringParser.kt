04aeba1c7aef58a093b781dee64419f9befc9b3b
viktorlysenko <viktor.lysenco@gmail.com>
2021-05-26 01:36:13 +0300

    feat: add task StringParser

--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -2,8 +2,67 @@ package subtask3
 
 class StringParser {
 
-    // TODO: Complete the following function
+    /*fun getSubstring(subString: String, i: String,resultArray: ArrayList<String>): String{
+        var stringBefo:String = subString.substringBefore(i)
+        //println("Befo $stringBefo")
+        stringBefo =
+            when(i) {
+            ")" -> stringBefo.substringAfter("(")
+             "]" -> stringBefo.substringAfter("[")
+             ">" -> stringBefo.substringAfter("<")
+        else -> {
+            println("Error")
+            ""
+        }
+        }
+        println("Befo $stringBefo")
+        var stringAfter = subString.substringAfter(i)
+        println("After $stringAfter")
+        val afterafter = stringAfter.substringAfter(i)
+        println("After-after $afterafter")
+        if (stringAfter != stringBefo && afterafter != stringAfter)
+        getSubstring(stringAfter, i,resultArray)
+        resultArray.add(stringBefo)
+        return stringBefo//stringAfter
+    }*/
+    fun getSubstring(subString: String, i: String,resultArray: ArrayList<String>): String{
+        var stringAfter:String = subString.substringAfter(i)
+        var stringBefo:String =
+            when(i) {
+                "(" -> stringAfter.substringBefore(")")
+                "[" -> stringAfter.substringBefore("]")
+                "<" -> stringAfter.substringBefore(">")
+                else -> {
+                    println("Error")
+                    ""
+                }
+            }
+        println(stringBefo)
+        resultArray.add(stringBefo)
+        return stringBefo//stringAfter
+    }
+
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        var ResultStrigArray : ArrayList<String> = arrayListOf()
+      var i:Int = 0
+       while(i < inputString.length - 1) {
+           if(inputString[i]=='(' || inputString[i]=='[' || inputString[i]=='<')
+               getSubstring(inputString.substring(i),inputString[i].toString(),ResultStrigArray)
+
+
+            i+=1
+       }
+        //println(ResultStrigArray)
+        return ResultStrigArray.toTypedArray()
+     /*   var ResultStrigArray : ArrayList<String> = arrayListOf()
+      for(i in ">])") {
+
+          println(i)
+          var st: String = getSubstring(inputString,i.toString(),ResultStrigArray)
+          println(ResultStrigArray)
+         // ResultStrigArray.add(st)
+      }
+        return ResultStrigArray.toTypedArray()*/
+
     }
 }
