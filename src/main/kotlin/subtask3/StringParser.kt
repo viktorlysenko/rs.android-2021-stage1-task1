6c3959d77aef81e664de1f8587a3917e61371ba7
viktorlysenko <viktor.lysenco@gmail.com>
2021-05-26 21:27:14 +0300

    refactor: change algorithm string parser

--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -2,67 +2,58 @@ package subtask3
 
 class StringParser {
 
-    /*fun getSubstring(subString: String, i: String,resultArray: ArrayList<String>): String{
-        var stringBefo:String = subString.substringBefore(i)
-        //println("Befo $stringBefo")
-        stringBefo =
-            when(i) {
-            ")" -> stringBefo.substringAfter("(")
-             "]" -> stringBefo.substringAfter("[")
-             ">" -> stringBefo.substringAfter("<")
-        else -> {
-            println("Error")
-            ""
+    fun findCloseBracketIndex(subString: String, openDelimiter: String, closeDelimiter: String): Int{
+        var bracketLevel: Int = 1
+        var lastPositionSt: Int = -1
+        for( index in 0 .. subString.length - 1) {
+            when (subString[index].toString()){
+                openDelimiter->bracketLevel+=1
+                closeDelimiter-> {
+                    bracketLevel -= 1
+                    lastPositionSt = index
+                }
+            }
+
+            if (bracketLevel == 0) return lastPositionSt
         }
-        }
-        println("Befo $stringBefo")
-        var stringAfter = subString.substringAfter(i)
-        println("After $stringAfter")
-        val afterafter = stringAfter.substringAfter(i)
-        println("After-after $afterafter")
-        if (stringAfter != stringBefo && afterafter != stringAfter)
-        getSubstring(stringAfter, i,resultArray)
-        resultArray.add(stringBefo)
-        return stringBefo//stringAfter
-    }*/
+        return lastPositionSt
+    }
+
     fun getSubstring(subString: String, i: String,resultArray: ArrayList<String>): String{
         var stringAfter:String = subString.substringAfter(i)
         var stringBefo:String =
             when(i) {
-                "(" -> stringAfter.substringBefore(")")
-                "[" -> stringAfter.substringBefore("]")
-                "<" -> stringAfter.substringBefore(">")
+                "(" -> {
+                    var closeBIndex: Int = findCloseBracketIndex(stringAfter,i,")")
+                    stringAfter.substring(0, closeBIndex)
+                }
+                "[" -> {
+                    var closeBIndex: Int = findCloseBracketIndex(stringAfter,i,"]")
+                    stringAfter.substring(0, closeBIndex)
+                }
+                "<" -> {
+                    var closeBIndex: Int = findCloseBracketIndex(stringAfter,i,">")
+                    stringAfter.substring(0, closeBIndex)
+                }
                 else -> {
                     println("Error")
                     ""
                 }
             }
-        println(stringBefo)
+       // println(stringBefo)
         resultArray.add(stringBefo)
-        return stringBefo//stringAfter
+        return stringBefo
     }
 
     fun getResult(inputString: String): Array<String> {
-        var ResultStrigArray : ArrayList<String> = arrayListOf()
+      var ResultStrigArray : ArrayList<String> = arrayListOf()
       var i:Int = 0
        while(i < inputString.length - 1) {
            if(inputString[i]=='(' || inputString[i]=='[' || inputString[i]=='<')
                getSubstring(inputString.substring(i),inputString[i].toString(),ResultStrigArray)
 
-
             i+=1
        }
-        //println(ResultStrigArray)
         return ResultStrigArray.toTypedArray()
-     /*   var ResultStrigArray : ArrayList<String> = arrayListOf()
-      for(i in ">])") {
-
-          println(i)
-          var st: String = getSubstring(inputString,i.toString(),ResultStrigArray)
-          println(ResultStrigArray)
-         // ResultStrigArray.add(st)
-      }
-        return ResultStrigArray.toTypedArray()*/
-
     }
 }
